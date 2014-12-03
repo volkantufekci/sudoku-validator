9dece9878d24b51bc1fa56cb1976d2649e0f00b5
volkantufekci <tufekcivolkan@gmail.com>
2014-12-03 23:28:16 +0200

    Reading whole line implemented Stop watch lap is used to discriminate file read time from total time log level for invalid solutions are decreased to debug

--- src/main/java/com/volkan/sudoku/App.java
+++ src/main/java/com/volkan/sudoku/App.java
@@ -5,8 +5,6 @@ import org.perf4j.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -30,15 +28,17 @@ public class App
 
     public static void main( String[] args ) throws IOException {
         App app = new App();
-        StopWatch stopWatch = new LoggingStopWatch("File Parse");
         app.parseTheSolutionFile();
-        stopWatch.stop();
     }
 
     private void parseTheSolutionFile() throws IOException {
-//        FileReader reader = new FileReader("/Users/vol/Downloads/samples.txt");
+        StopWatch stopWatch = new LoggingStopWatch();
+
+//        BufferedReader reader = new BufferedReader(new FileReader("/Users/vol/Downloads/samples.txt"));
         List<String> lines = Files.readAllLines(
                 Paths.get("/Users/vol/Downloads/samples.txt"), StandardCharsets.UTF_8);
+
+        stopWatch.lap("file read");
 
         int availableCpuCount = Runtime.getRuntime().availableProcessors();
         ExecutorService executorService = Executors.newFixedThreadPool(availableCpuCount);
@@ -55,8 +55,8 @@ public class App
             }
 
             if (isValidLine(line)) {
-//                executorService.submit(validateSolution(line, lineNumber));
-                validateSolution2(line, lineNumber);
+                executorService.submit(validateSolution(line, lineNumber));
+//                validateSolution2(line, lineNumber);
             } else {
                 invalidCount.incrementAndGet();
                 logger.warn("The line #{} is not well formed: {}", lineNumber, line);
@@ -74,6 +74,7 @@ public class App
         int validLineCount = validLineCount(invalidCount.get(), totalLineCountOfSolutions);
         logger.info("{} solutions are read! Valid Solution Count={}, Invalid Solution Count={}",
                 totalLineCountOfSolutions, validLineCount, invalidCount.get());
+        stopWatch.stop("Whole process");
     }
 
     private int validLineCount(int invalidLineCount, int totalLineCountOfSolutions) {
@@ -88,25 +89,24 @@ public class App
         Solution solution = new Solution(line);
         if (!solution.isValid()) {
             invalidCount.incrementAndGet();
-            logger.info("Line #{} is an invalid solution: {}", lineNumber, line);
+            logger.debug("Line #{} is an invalid solution: {}", lineNumber, line);
         }
     }
 
     private Callable validateSolution(final String line, final int lineNumber) {
-        Callable callable = new Callable() {
+
+        return new Callable() {
             @Override
             public Object call() throws Exception {
                 Solution solution = new Solution(line);
                 if (!solution.isValid()) {
                     invalidCount.incrementAndGet();
-                    logger.info("Line #{} is an invalid solution: {}", lineNumber, line);
+                    logger.debug("Line #{} is an invalid solution: {}", lineNumber, line);
                 }
 
                 return null;
             }
         };
-
-        return callable;
     }
 
     private boolean shouldLineBeSkipped(String line) {
